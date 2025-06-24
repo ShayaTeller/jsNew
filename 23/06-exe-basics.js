52ba256f5edf7eebb2ca60aa49c3717beffa0262
david teller <ydt009@gmail.com>
2025-06-25 00:19:22 +0300

    basics-exe

---
+++ 23/06-exe-basics.js
@@ -0,0 +1,44 @@
+
+// // exe 1
+
+// let a = `some string`;
+// let b = `other string`;
+
+// console.log(a,b)
+// a  = a +" " +b
+// b = a
+
+// b = b.substring(0,a.length/2)
+// a = a.substring(b.length,a.length)
+// console.log(a)
+// console.log(b)
+
+// // exe 3
+// let family = { parents: {}, children: [ {name: "Ali"}, {name: "Lea"}, {name: "Mona"} ] };
+
+// console.log(family["children"])
+
+// for (let index = 0; index < family["children"].length; index++) {
+//     if(index==2){
+//         family["children"][index]["name"] = "shaya"
+//     }    
+// }
+// console.log(family["children"])
+
+// // exe 4 
+
+// arr = [1,2,3,4]
+// newArrey = []
+//  for (let index = arr.length-1; index >= 0; index--) {
+//     const element = arr[index];
+//     newArrey.push(element)
+    
+// }
+// console.log(newArrey)
+
+// exe 5 לא עובד עצרתי באמצע
+// const arr = [5,6,4,"h","j","f","f"]
+// let counter = 0;
+// narr = arr.filter((elemnt)=>{if(typeof elemnt==typeof elemnt+1){counter++}return counter} )
+
+// console.log(narr)
